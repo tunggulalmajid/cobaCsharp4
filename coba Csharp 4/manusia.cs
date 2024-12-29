3643447a61fd950bcc5677e3067fa28d47ea906b
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-30 01:11:03 +0700

    Add project files.

---
+++ coba Csharp 4/manusia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coba_Csharp_4
+{
+    class manusia
+    {
+        int umur;
+        string nama;
+        public void sayHi()
+        {
+            Console.WriteLine("halo semua");
+        }
+
+    }
+}
